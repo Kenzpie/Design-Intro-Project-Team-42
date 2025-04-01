9735412180e2e9d279c01f300a46c4df0f1e1858
seh706 <seh706@msstate.edu>
2025-04-01 13:49:20 -0500

    Elegoo_TempSensor_20250401134428.ino

--- Elegoo_TempSensor_20250401134428.ino
+++ Elegoo_TempSensor_20250401134428.ino
@@ -1,14 +1,15 @@
+//library
 #include <dht11.h>
 #define DHT11PIN 2
 
 dht11 DHT11;
 
-void  setup()
+void setup()
 {
   Serial.begin(9600);
- 
 }
 
+//reads DHT11 and shows humidity and temperature in Fahrenheit
 void loop()
 {
   Serial.println();
@@ -18,9 +19,11 @@ void loop()
   Serial.print("Humidity (%): ");
   Serial.println((float)DHT11.humidity, 2);
 
-  Serial.print("Temperature  (C): ");
-  Serial.println((float)DHT11.temperature, 2);
+  float tempC = (float)DHT11.temperature;
+  float tempF = (tempC * 9.0 / 5.0) + 32.0;
+
+  Serial.print("Temperature (F): ");
+  Serial.println(tempF, 2);
 
   delay(1000);
-
 }
