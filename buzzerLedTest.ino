082e21bca3faafafca145991d03c993f3e089dac
Kenzpie <ams2083@msstate.edu>
2025-04-07 16:23:59 -0500

    Update buzzerLedTest.ino

--- buzzerLedTest.ino
+++ buzzerLedTest.ino
@@ -1,22 +1,32 @@
 /*Wap to interface LED and Buzzer with Arduino board, so that buzzer
   is put on whenever LED is on and Buzzer is put off when LED is off.
 */
-const int buzzer = 6;
-const int led = 10;
+
+#define BUZZER_PIN 6    // Pin connected to the buzzer
+#define LED_PIN 10      // Pin connected to the LED
+
 void setup() {
-  // put your setup code here, to run once:
-  pinMode(buzzer, INPUT);
-  pinMode(led, OUTPUT);
+  pinMode(BUZZER_PIN, OUTPUT);  // Set buzzer pin as output
+  pinMode(LED_PIN, OUTPUT);     // Set LED pin as output
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-  digitalWrite(buzzer, HIGH);
-  digitalWrite(led, HIGH);
-  tone(buzzer, 100);
-  delay(1000);
-  digitalWrite(buzzer, LOW);
-  digitalWrite(led, LOW);
-  noTone(buzzer);
-  delay(1000);
+  // Check if the temperature is above 100 & less than 103
+  if ((tempF > 100.0) && (tempF < 103.0)) {
+    digitalWrite(BUZZER_PIN, LOW);  // Turn buzzer off
+    digitalWrite(LED_PIN, HIGH);     // Turn LED on if temperature is above threshold
+  } 
+  // Check if the temperature is above 102
+
+  else if(tempF > 102.0){
+    digitalWrite(BUZZR_PIN, HIGH);   // Turn buzzer off if temperature is below threshold
+    digitalWrite(LED_PIN, HIGH);      // Turn LED off if temperature is below threshold
+  } 
+   // Check if the temperature is less than 101
+
+  else {
+    digitalWrite(BUZZR_PIN, LOW);   // Turn buzzer off if temperature is below threshold
+    digitalWrite(LED_PIN, LOW);     // Turn
+  }
+
+
 }
